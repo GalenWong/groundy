8cca471428b1e1f72ff047e3bbb38bba325be721
John Tran <johntran627@gmail.com>
2020-10-29 18:16:40 -0700

    fixed

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,15 +1,13 @@
 interface Song {
-	title: string; 
-	channel: string; 
-	ytID: string;
-	downloaded: boolean;
-	fileName: string; 	
-}; 
-
-interface Playlist {
-	id: string;
-	name: string; 
-	songs: Song[];
+  title: string;
+  channel: string;
+  ytID: string;
+  downloaded: boolean;
+  fileName: string;
 }
 
-
+interface Playlist {
+  id: string;
+  name: string;
+  songs: Song[];
+}
