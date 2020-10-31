fb24cb3de3d5fed3f39546c36d0f641a494ecdd8
Tony Hu <tonyhu3085@gmail.com>
2020-10-31 16:33:14 -0700

    mocks

--- app/utils/mocks.ts
+++ app/utils/mocks.ts
@@ -1,1 +1,17 @@
 /* write mocks here */
+
+const newPlaylist = async (name: string): Promise<string> => {
+  return `Playlist ${name} created`;
+};
+
+const startDownload = async (ytID: string): Promise<void> => {};
+
+const deleteSong = async (ytID: string): Promise<string> => {
+  return `${ytID} deleted`;
+};
+
+const getSongPath = async (ytID: string): Promise<string> => {
+  return `C:/Users/User/Music/example_${ytID}.mp3`;
+};
+
+export { newPlaylist, startDownload, deleteSong, getSongPath };
