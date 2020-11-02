d2c63a2bb523c35e4143af9da1887942d6ea5c47
GalenWong <wonggalen1999@gmail.com>
2020-11-02 11:37:26 +0800

    working toy player

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -75,9 +75,11 @@ const createWindow = async () => {
       process.env.ERB_SECURE !== 'true'
         ? {
             nodeIntegration: true,
+            webSecurity: false, // for accessing local music file
           }
         : {
             preload: path.join(__dirname, 'dist/renderer.prod.js'),
+            webSecurity: false, // for accessing local music file
           },
   });
 
