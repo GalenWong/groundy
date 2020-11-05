97a7d4d23b1da4715565cf57f12b1be29c511687
Jingle Huang <jingleh@g.ucla.edu>
2020-11-05 16:22:25 -0500

    reduce some warnings

--- test/e2e/HomePage.e2e.ts
+++ test/e2e/HomePage.e2e.ts
@@ -1,17 +1,20 @@
 /* eslint jest/expect-expect: off, jest/no-test-callback: off */
-import { ClientFunction, Selector } from 'testcafe';
+import {
+  ClientFunction,
+  // ,Selector
+} from 'testcafe';
 
-const getPageUrl = ClientFunction(() => window.location.href);
+// const getPageUrl = ClientFunction(() => window.location.href);
 const getPageTitle = ClientFunction(() => document.title);
-const counterSelector = Selector('[data-tid="counter"]');
-const buttonsSelector = Selector('[data-tclass="btn"]');
-const clickToCounterLink = (t) =>
-  t.click(Selector('a').withExactText('to Counter'));
-const incrementButton = buttonsSelector.nth(0);
-const decrementButton = buttonsSelector.nth(1);
-const oddButton = buttonsSelector.nth(2);
-const asyncButton = buttonsSelector.nth(3);
-const getCounterText = () => counterSelector().innerText;
+// const counterSelector = Selector('[data-tid="counter"]');
+// const buttonsSelector = Selector('[data-tclass="btn"]');
+// const clickToCounterLink = (t) =>
+//   t.click(Selector('a').withExactText('to Counter'));
+// const incrementButton = buttonsSelector.nth(0);
+// const decrementButton = buttonsSelector.nth(1);
+// const oddButton = buttonsSelector.nth(2);
+// const asyncButton = buttonsSelector.nth(3);
+// const getCounterText = () => counterSelector().innerText;
 const assertNoConsoleErrors = async (t) => {
   const { error } = await t.getBrowserConsoleMessages();
   await t.expect(error).eql([]);
