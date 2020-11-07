dbde07d88788817d26cc8c0d3a5a105a2571319c
Jingle Huang <jingleh@g.ucla.edu>
2020-11-07 17:49:53 -0500

    add mui

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -1,14 +1,15 @@
 /* eslint react/jsx-props-no-spreading: off */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import routes from './constants/routes.json';
+import routesJSON from './constants/routes.json';
 import App from './containers/App';
 // import HomePage from './containers/HomePage';
 import DownloadedPage from './containers/DownloadedPage';
 import RecommendedPage from './containers/RecommendedPage';
 import FindRelatedPage from './containers/FindRelatedPage';
 import AllPlaylistsPage from './containers/AllPlaylistsPage';
-import SideBar from './components/Sidebar';
+import SettingPage from './containers/SettingPage';
+
 // Lazily load routes and code split with webpack
 // const LazyCounterPage = React.lazy(() =>
 //   import(/* webpackChunkName: "CounterPage" */ './containers/CounterPage')
@@ -20,17 +21,24 @@ import SideBar from './components/Sidebar';
 //   </React.Suspense>
 // );
 
+interface RoutesObject {
+  [index: string]: string;
+}
+const routes: RoutesObject = routesJSON;
+
 export default function Routes() {
   return (
     <App>
-      <SideBar />
+      <br />
+      <br />
+      <br />
+      <br />
       <Switch>
-        {/* <Route path={routes.COUNTER} component={CounterPage} />
-        <Route path={routes.HOME} component={HomePage} /> */}
-        <Route path={routes.ALLPLAYLISTS} component={AllPlaylistsPage} />
-        <Route path={routes.FINDRELATED} component={FindRelatedPage} />
-        <Route path={routes.RECOMMENDED} component={RecommendedPage} />
-        <Route path={routes.DOWNLOADED} component={DownloadedPage} />
+        <Route path={routes['All Playlists']} component={AllPlaylistsPage} />
+        <Route path={routes['Find Related']} component={FindRelatedPage} />
+        <Route path={routes.Recommended} component={RecommendedPage} />
+        <Route path={routes.Downloaded} component={DownloadedPage} />
+        <Route path={routes.Setting} component={SettingPage} />
       </Switch>
     </App>
   );
