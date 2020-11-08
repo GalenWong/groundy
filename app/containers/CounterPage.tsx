d79b771baf6e592adaad2f8738c0521bb274bfdc
Jingle Huang <jingleh@g.ucla.edu>
2020-11-07 19:08:07 -0500

    remove unused

--- app/containers/CounterPage.tsx
+++ app/containers/CounterPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 // import Counter from '../features/counter/Counter';
 
 export default function CounterPage() {
--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 // import Home from '../components/Home';
 
 export default function HomePage() {
