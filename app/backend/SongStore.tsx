a1fe07b5f2989f9aff91d41ad11515bcad901b44
John Tran <johntran627@gmail.com>
2020-11-12 11:27:23 -0800

    updated

--- app/backend/SongStore.tsx
+++ app/backend/SongStore.tsx
@@ -1,39 +1,45 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
+import * as util from 'util';
+
+const unlink = util.promisify(fs.unlink);
+const readdir = util.promisify(fs.readdir);
 
 export default class SongStore {
   private static instance: SongStore;
 
-  private storageDirectory: string;
+  private static storageDirectory: string;
 
   private constructor(storageDirectory: string) {
     this.storageDirectory = storageDirectory;
   }
 
-  static getInstance(storageDirectory: string): SongStore {
-    if (!SongStore.instance) {
-      SongStore.instance = new SongStore(storageDirectory);
-    }
+  static getInstance(): SongStore {
+    if (!this.storageDirectory) return null;
     return SongStore.instance;
   }
 
-  getWriteStream(nameOfFile: string): any {
-    if (!this.storageDirectory) this.storageDirectory = process.cwd();
+  static setInstance(storageDirectory: string): void {
+    this.storageDirectory = storageDirectory;
+    SongStore.instance = new SongStore(storageDirectory);
+  }
+
+  getWriteStream(nameOfFile: string): WriteStream {
     const filePath = path.join(this.storageDirectory, nameOfFile);
     return fs.createWriteStream(filePath);
   }
 
-  async delete(nameOfFile: string): Promise<any> {
+  async delete(nameOfFile: string): Promise<void> {
     const filePath = path.join(this.storageDirectory, nameOfFile);
-    await fs.unlink(filePath);
+    await unlink(filePath);
   }
 
   getStorageDirectory(): string {
     return this.storageDirectory;
   }
 
-  async getAllSongs(): Promise<any> {
-    const songs = await fs.readdir(this.storageDirectory);
+  async getAllSongs(): string[] {
+    const songs = await readdir(this.storageDirectory);
     return songs;
   }
 }
--- test/songstore/songstore.spec.ts
+++ test/songstore/songstore.spec.ts
@@ -1,37 +1,81 @@
 import { assert } from 'console';
+import * as path from 'path';
+import mock from 'mock-fs';
 import SongStore from '../../app/backend/SongStore';
 
 const os = require('os');
 const fs = require('fs');
+const writeFile = require('util').promisify(fs.writeFile);
 
 jest.mock('fs');
 
 describe('songstore', () => {
   it('getInstance', () => {
     const tempDir = os.tmpdir();
-    expect(SongStore.getInstance(tempDir)).toBeInstanceOf(SongStore);
+    SongStore.setInstance(tempDir);
+    expect(SongStore.getInstance()).toBeInstanceOf(SongStore);
   });
 
   it('getStorageDirectory', () => {
     const tempDir = os.tmpdir();
-    assert(SongStore.getInstance(tempDir).getStorageDirectory(), tempDir);
+    SongStore.setInstance(tempDir);
+    assert(SongStore.getInstance().getStorageDirectory(), tempDir);
   });
 
   it('getWriteStream', () => {
     const tempDir = os.tmpdir();
-    SongStore.getInstance(tempDir).getWriteStream('dummy.txt');
+    SongStore.setInstance(tempDir);
+    SongStore.getInstance().getWriteStream('dummy.txt');
     expect(fs.createWriteStream).toHaveBeenCalled();
   });
 
   it('delete', () => {
     const tempDir = os.tmpdir();
-    SongStore.getInstance(tempDir).delete('dummy.txt');
+    SongStore.setInstance(tempDir);
+    SongStore.getInstance().delete('dummy.txt');
     expect(fs.unlink).toHaveBeenCalled();
   });
 
   it('getAllSongs', () => {
     const tempDir = os.tmpdir();
-    SongStore.getInstance(tempDir).getAllSongs();
+    SongStore.setInstance(tempDir);
+    SongStore.getInstance().getAllSongs();
     expect(fs.readdir).toHaveBeenCalled();
   });
+
+  // it('getAllSongs', async () => {
+  //   const tempDir = os.tmpdir();
+  //   SongStore.setInstance(tempDir);
+  //   const filePath = path.join(tempDir, 'dummy.txt');
+  //   writeFile(filePath, 'hello world');
+  //   const songs = await SongStore.getInstance().getAllSongs();
+  //   console.log(songs);
+  // });
+
+  // it('getAllSongs', async () => {
+  //   const tempDir = os.tmpdir();
+  //   SongStore.setInstance(tempDir);
+  //   const filePath = path.join(tempDir, 'dummy.txt');
+  //   fs.writeFileSync(filePath, 'hello world');
+  //   const songs = await SongStore.getInstance().getAllSongs();
+  //   console.log(songs);
+  // });
+
+  // it('delete', async () => {
+  //   const tempDir = os.tmpdir();
+  //   SongStore.setInstance(tempDir);
+  //   const filePath = path.join(tempDir, 'dummy.txt');
+  //   writeFile(filePath, 'hello world');
+  //   await SongStore.getInstance().delete('dummy.txt');
+
+  // });
+
+  // it('getWriteStream', async () => {
+  //   const tempDir = os.tmpdir();
+  //   SongStore.setInstance(tempDir);
+  //   const filePath = path.join(tempDir, 'dummy.txt');
+  //   writeFile(filePath, 'hello world');
+  //   const w = await SongStore.getInstance().getWriteStream('dummy.txt');
+  //   //expect(w).toBeInstanceOf(fs.WriteStream);
+  // });
 });
