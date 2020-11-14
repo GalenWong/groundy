ab1d2098971b70b403bc21ed5f16a3449add1677
Jingle Huang <jingleh@g.ucla.edu>
2020-11-14 12:02:15 -0500

    typedoc config

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,3 +1,6 @@
+/**
+ * @typeParam Song - Contains info of a local/online song
+ */
 export interface Song {
   title: string;
   channel: string;
@@ -6,12 +9,20 @@ export interface Song {
   fileName?: string;
 }
 
+/**
+ * @typeParam Playlist - Contains info of a local playlist
+ *
+ */
 export interface Playlist {
   id: string;
   name: string;
   songs: Song[];
 }
 
+/**
+ * @typeParam DownloadedSong - Used for downloaded songs
+ *
+ */
 export interface DownloadedSong extends Song {
   downloaded: true;
   filePath: string;
