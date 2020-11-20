10c6ecc251b3fbf4b9ccd01a096378fe3825f885
GalenWong <wonggalen1999@gmail.com>
2020-11-20 13:12:44 +0800

    useEffect to trigger best play song implementation

--- app/containers/PlayerWrapper/index.tsx
+++ app/containers/PlayerWrapper/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { ReactJkMusicPlayerInstance } from 'react-jinke-music-player';
 import MusicPlayer from '../../components/MusicPlayer';
 import { DownloadedSong } from '../../types';
@@ -31,12 +31,27 @@ export default function PlayerWrapper({ children }: PlayerWrapperProps) {
   const playerInstanceRef = useRef<ReactJkMusicPlayerInstance>();
 
   const [playlist, setPlaylist] = useState<DownloadedSong[]>([]);
+  const [playIndex, setPlayIndex] = useState(0);
 
-  const playSong = (song: DownloadedSong) => {
-    setPlaylist([song, ...playlist]);
+  // trigger setting play index
+  useEffect(() => {
     if (playerInstanceRef.current && playerInstanceRef.current.updatePlayIndex)
-      playerInstanceRef.current?.updatePlayIndex(0);
-  };
+      playerInstanceRef.current?.updatePlayIndex(playIndex);
+  }, [playIndex]);
+
+  const playSong = useCallback(
+    (song: DownloadedSong) => {
+      const newPlaylist = [
+        ...playlist.slice(0, playIndex + 1),
+        song,
+        ...playlist.slice(playIndex + 1),
+      ];
+      setPlaylist(newPlaylist);
+      // delay setting playIndex when the state has updated to trigger effect
+      setPlayIndex(playIndex + 1);
+    },
+    [playIndex, playlist]
+  );
 
   const addSongToQueue = (song: DownloadedSong) => {
     setPlaylist([...playlist, song]);
@@ -62,6 +77,7 @@ export default function PlayerWrapper({ children }: PlayerWrapperProps) {
         getAudioInstance={(audio) => {
           playerInstanceRef.current = audio;
         }}
+        onPlayIndexChange={(i) => setPlayIndex(i)}
       />
     </>
   );
