4b3bc192b6674a05bb37cce88d6e4e10f5708885
Tony Hu <tonyhu3085@gmail.com>
2020-11-23 22:08:42 -0800

    recommended page

--- app/components/Recommended/index.tsx
+++ app/components/Recommended/index.tsx
@@ -1,5 +1,60 @@
-import React from 'react';
+import * as React from 'react';
+import { Box, Typography, Paper, makeStyles, Button } from '@material-ui/core';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import Playlist from '../Playlist';
+import { DownloadedSong, Song, Progress } from '../../types';
+import { getAllRecommendation } from '../../utils';
+import { playerQueueContext } from '../../containers/PlayerWrapper/index';
 
-export default function Recommended(): JSX.Element {
-  return <div> The Recommended Page.</div>;
+const useStyles = makeStyles({
+  topbar: {
+    display: 'flex',
+  },
+  fillSpace: {
+    flex: '1',
+  },
+  padded: {
+    padding: '10px',
+  },
+});
+
+interface State {
+  songs: (DownloadedSong | Song)[];
+  downloads: Record<string, Progress>;
 }
+
+const Recommended = () => {
+  const [state, setState] = React.useState<State>({ songs: [], downloads: {} });
+  const { songs, downloads } = state;
+  // const controls = React.useContext(playerQueueContext);
+  React.useEffect(() => {
+    const getData = async () => {
+      getAllRecommendation()
+        .then((s: (DownloadedSong | Song)[]) =>
+          setState({ songs: s, downloads: state.downloads })
+        )
+        .catch(() => {});
+    };
+    getData();
+  }, []);
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.padded}>
+      <Box className={classes.topbar}>
+        <Typography className={classes.fillSpace} variant="h4" noWrap>
+          Recommended
+        </Typography>
+        <Box>
+          <Button>
+            <Typography variant="caption">Action</Typography>
+            <PlayArrowIcon />
+          </Button>
+        </Box>
+      </Box>
+      <Playlist songs={songs} downloads={downloads} />
+    </Paper>
+  );
+};
+
+export default Recommended;
