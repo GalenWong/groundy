5553bd393ca79c5d0c3f887a388b7d6a522c6089
GalenWong <wonggalen1999@gmail.com>
2020-11-30 07:46:17 +0800

    resolve songs for get all downloads

---
+++ test/backend/ipc/getAllDownloads.i.spec.ts
@@ -0,0 +1,40 @@
+import os from 'os';
+import path from 'path';
+import getAllDownloads from '../../../app/backend/ipc/getAllDownloads';
+import Database from '../../../app/backend/database';
+import SongStore from '../../../app/backend/SongStore';
+import { DownloadedSong } from '../../../app/types';
+
+describe('getAllDownloads', () => {
+  it('resolve file path', async () => {
+    const tempDir = os.tmpdir() + Math.random();
+    const db = Database.getInstance(tempDir);
+    SongStore.getInstance().setDirectory(tempDir);
+    await db.createSong({
+      ytID: 'my_dummy_id',
+      fileName: 'dummy.mp3',
+      channel: 'fake_channel',
+      title: 'fake_title',
+      downloaded: true,
+    });
+    await db.createSong({
+      ytID: 'my_dummy_id_2',
+      fileName: undefined,
+      channel: 'fake_channel',
+      title: 'fake_title',
+      downloaded: false,
+    });
+
+    const songs = await getAllDownloads();
+    const song1List = songs.filter((v) => v.ytID === 'my_dummy_id');
+    expect(song1List.length).toBe(1);
+    expect((song1List[0] as DownloadedSong).filePath).toBe(
+      path.join(tempDir, 'dummy.mp3')
+    );
+    expect(song1List[0].downloaded).toBe(true);
+
+    const song2List = songs.filter((v) => v.ytID === 'my_dummy_id_2');
+    expect(song2List.length).toBe(1);
+    expect(song2List[0].downloaded).toBe(false);
+  });
+});
